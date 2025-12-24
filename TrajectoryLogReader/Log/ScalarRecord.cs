9fabd778ae3e7ec0cee2feadf0ea4ed202b79f8b
anmcgrath <andrew.n.mcgrath@gmail.com>
2025-12-24 13:19:37 +1100

    Add conversions in scalar record

--- TrajectoryLogReader/Log/ScalarRecord.cs
+++ TrajectoryLogReader/Log/ScalarRecord.cs
@@ -23,4 +23,34 @@ public class ScalarRecord
     /// Actual - Expected
     /// </summary>
     public float Delta => Scale.Delta(_log.Header.AxisScale, Expected, _log.Header.AxisScale, Actual, _axis);
+
+    /// <summary>
+    /// Returns the expected value in the coord system/scale <paramref name="axisScale"/>
+    /// </summary>
+    /// <param name="axisScale"></param>
+    /// <returns></returns>
+    public float ExpectedInScale(AxisScale axisScale) =>
+        Scale.Convert(_log.Header.AxisScale, axisScale, _axis, Expected);
+
+    /// <summary>
+    /// Returns the actual value in the coord system/scale <paramref name="axisScale"/>
+    /// </summary>
+    /// <param name="axisScale"></param>
+    /// <returns></returns>
+    public float ActualInScale(AxisScale axisScale) =>
+        Scale.Convert(_log.Header.AxisScale, axisScale, _axis, Actual);
+
+    /// <summary>
+    /// Returns the expected value in IEC scale
+    /// </summary>
+    /// <returns></returns>
+    public float ExpectedInIec() =>
+        Scale.ToIec(_log.Header.AxisScale, _axis, Expected);
+
+    /// <summary>
+    /// Returns the actual value in IEC scale
+    /// </summary>
+    /// <returns></returns>
+    public float ActualInIec() =>
+        Scale.ToIec(_log.Header.AxisScale, _axis, Actual);
 }
