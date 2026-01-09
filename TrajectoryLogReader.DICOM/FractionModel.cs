7dadce5ad2e380425497faf5858c27f213af963d
anmcgrath <andrew.n.mcgrath@gmail.com>
2026-01-09 14:44:16 +1100

    Add beam fractionation

---
+++ TrajectoryLogReader.DICOM/FractionModel.cs
@@ -0,0 +1,8 @@
+namespace TrajectoryLogReader.DICOM;
+
+public class FractionModel
+{
+    public int FractionGroupNumber { get; set; }
+    public int NumberOfFractionsPlanned { get; set; }
+    public int NumberOfBeams { get; set; }
+}
